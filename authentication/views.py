f81afd7186745b15b8d208e0df400cc1fcb66146
Rachel ShenHom <rachelshenhom@Rachels-Air.fios-router.home>
2020-09-16 14:18:00 -0400

    addedAuthView

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
+from .serializers import RegistrationSerializer, LoginSerializer, UserListSerializer
+from .models import User
+class RegistrationAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
+    def post(self, request):
+        user = request.data.get('user', {})
+        if not user:
+            user = {
+                "username": request.data.get('username'),
+                "password": request.data.get('password')
+            }
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-# Create your views here.
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+    def post(self, request):
+        user = request.data.get('user', {})
+        # this is base case if user is not created:
+        if not user:
+            user = {
+                "username": request.data.get('username'),
+                "password": request.data.get('password'),
+            }
+        serializer = self.serializer_class(data=user)
+        # raise an error if it doesn't go right
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
