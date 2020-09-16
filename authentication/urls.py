18a3c86907076facaa424112839962e17d1b5153
Rachel ShenHom <rachelshenhom@Rachels-Air.fios-router.home>
2020-09-16 18:08:14 -0400

    migration

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.urls import path, include
 from .views import RegistrationAPIView, LoginAPIView
+
+
 urlpatterns = [
     url(r'^users/register/$', RegistrationAPIView.as_view(), name='register'),
     url(r'^users/login/$', LoginAPIView.as_view(), name='login'),
-    path('', include('apps.api.urls'))
+    # path('', include('apps.api.urls'))
 ]
